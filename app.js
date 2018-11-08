8f1133db611d985f53670e70f573167e8f4c9dab
AndyYang <a60803ndy@gmail.com>
2018-11-08 19:18:12 +1300

    n

--- app.js
+++ app.js
@@ -103,3 +103,75 @@ module.exports = app;
 
 
 
+
+// for CART
+//Dependencies
+const Joi = require('joi');
+const express = require('express')
+const path = require('path')
+const bodyParser= require('body-parser')
+const PORT = process.env.PORT || 8080
+
+var urlencodedParser= bodyParser.urlencoded({extended: false});
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+var app = express();
+var comments=[];
+
+
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  
+  
+  
+  
+  //**************GET********************/
+app.get('/api/products', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    var result = await client.query('SELECT * FROM product_table');   
+   
+    if (!result) {
+      return res.send('No data found');
+      }else{
+      result.rows.forEach(row=>{
+      console.log(row);
+      }); 
+      }
+
+  res.send(result.rows);
+  client.release();
+
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+
+
+
+
+
+
+app.get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+  
+
+
+
+
+
+
+
+
